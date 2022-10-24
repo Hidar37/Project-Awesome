5e22e3db786d83efd2fc715f29d3ffe9000f11ac
Haider Ahmad <hidarahmadjob@gmail.com>
2022-10-24 13:16:20 +0430

    Add html and some js.

---
+++ source/js/add-book.js
@@ -0,0 +1,5 @@
+const bookContainer = document.getElementById('book-store');
+const form = document.getElementById('adding-book-form');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const addBtn = document.getElementById('add-btn');
