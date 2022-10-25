d758d700a449d185c776326a17e9f9a977859f05
Haider Ahmad <hidarahmadjob@gmail.com>
2022-10-25 12:41:08 +0430

    Class concept add to project.

--- source/js/add-book.js
+++ source/js/add-book.js
@@ -1,3 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-trailing-spaces */
 
 const bookContainer = document.getElementById('book-store');
@@ -7,45 +9,63 @@ const bookAuthor = document.getElementById('author');
 // const addBtn = document.getElementById('add-btn');
 let bookList = [];
 
-// Adding Books
-function addBook() {
-  const bookData = JSON.parse(localStorage.getItem('book'));
-  bookData.forEach((item, index) => {
-    const bookDiv = document.createElement('div');
-    const bookItem = document.createElement('p');
-    const removeButton = document.createElement('button');
-    bookItem.appendChild(document.createTextNode(`${item.title} by ${item.author}`));
-    removeButton.setAttribute('class', 'remove');
-    removeButton.appendChild(document.createTextNode('remove'));
-    // Remove Button Logic
-    removeButton.addEventListener('click', () => {
-      bookData.splice(index, 1);
-      localStorage.setItem('book', JSON.stringify(bookData));
-      bookDiv.remove();
+class AwesomeBook {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  // Remove Button
+  removeBook(btnAdderss, bookArray, btnIndex, parentDiv) {
+    btnAdderss.addEventListener('click', () => {
+      bookArray.splice(btnIndex, 1);
+      localStorage.setItem('book', JSON.stringify(bookArray));
+      parentDiv.remove();
       window.location.reload();
     });
-    
-    bookDiv.append(bookItem, removeButton);
-    bookContainer.appendChild(bookDiv);
-  });
+  }
+
+  // Adding Books
+  addBook() {
+    const bookData = JSON.parse(localStorage.getItem('book'));
+    bookData.forEach((item, index) => {
+      const bookDiv = document.createElement('div');
+      const bookItem = document.createElement('p');
+      const removeButton = document.createElement('button');
+      bookItem.appendChild(document.createTextNode(`${item.title} by ${item.author}`));
+      removeButton.setAttribute('class', 'remove');
+      removeButton.appendChild(document.createTextNode('remove'));
+      // Remove Button Logic
+      this.removeBook(removeButton, bookData, index, bookDiv);
+      bookDiv.append(bookItem, removeButton);
+      bookContainer.appendChild(bookDiv);
+    });
+  }
+
+  // Displaying Books
+  displayBooks() {
+    const book = {
+      title: this.title.value,
+      author: this.author.value,
+    };
+    bookList.push(book);
+    localStorage.setItem('book', JSON.stringify(bookList));
+    bookContainer.innerHTML = '';
+    this.addBook();
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  }
 }
+
+const bookObj = new AwesomeBook(bookTitle, bookAuthor);
 
 if (localStorage.getItem('book') !== null) {
   bookList = JSON.parse(localStorage.getItem('book'));
-  addBook();
+  bookObj.addBook();
 }
 
 // Add Button and create local storage
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const book = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-  bookList.push(book);
-  localStorage.setItem('book', JSON.stringify(bookList));
-  bookContainer.innerHTML = '';
-  addBook();
-  bookTitle.value = '';
-  bookAuthor.value = '';
+  bookObj.displayBooks();
 });
